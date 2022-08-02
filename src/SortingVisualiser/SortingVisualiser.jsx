a33a8185026b0f57825efda9904d809aaf368c74
Pravin <prav.ch5@gmail.com>
2022-08-02 12:18:35 +0530

    implemented variable arrayBars

--- src/SortingVisualiser/SortingVisualiser.jsx
+++ src/SortingVisualiser/SortingVisualiser.jsx
@@ -5,9 +5,9 @@ import { getInsertionSortAniamtions } from '../Algorithms/InsertionSort';
 import {getMergeSortAnimations} from '../Algorithms/MergeSort';
 import { getQuickSortAnimations } from '../Algorithms/QuickSort';
 
-let ARRAY_BARS = 300;
-let c1 = "#ccf381"
-let c2 = "#ff01fb"
+let c1 = "#ccf381" //normal bar
+let c2 = "#d90429" //compared bar
+let c3 =  "#d2ccf5"
 
 export default class SortingVisualiser extends Component {
 
@@ -16,23 +16,43 @@ export default class SortingVisualiser extends Component {
       
     this.state = {
         array : [],
-        speed : 50
+        speed : 4,
+        arrayBars : 200
     };
 
     }
 
   componentDidMount(){
-    this.resetArray();
+    this.resetArray(this.state.arrayBars);
   }
 
-  resetArray() {
+  resetArray(ab) {
      const array = [];
-     for(let i=0;i<ARRAY_BARS;i++){
+     for(let i=0;i<ab;i++){
         array.push(randomValue(5,450));
      }
+     
      this.setState({array});
+
+     const arrayBars = document.getElementsByClassName('array-bar');
+
+   for(let i=0;i<arrayBars.length;i++)
+   arrayBars[i].style.backgroundColor = c1
+  
   }
 
+colorAll(l){
+
+  const arrayBars = document.getElementsByClassName('array-bar');
+
+   for(let i=0;i<arrayBars.length;i++){
+   
+    setTimeout(()=>{
+   arrayBars[i].style.backgroundColor = c3
+    },l*this.state.speed + i*4)
+   }
+
+}
 
   bubbleSort(){
     
@@ -70,7 +90,7 @@ export default class SortingVisualiser extends Component {
             }
 
         }
-
+        this.colorAll(animations.length)
 }
 
   insertionSort(){
@@ -106,7 +126,7 @@ export default class SortingVisualiser extends Component {
         }
 
     }
-   
+    this.colorAll(animations.length)
 }
 
 mergeSort(){
@@ -142,7 +162,7 @@ mergeSort(){
         }
 
     }
-
+    this.colorAll(animations.length)
 }
 
 quickSort(){
@@ -179,13 +199,25 @@ quickSort(){
 
     }
 
+    this.colorAll(animations.length)
+
 }
 
 handleSlideChange=(e)=>{
 
-  let speed = e.currentTarget.value;
-   this.setState({speed})
+  let v = e.currentTarget.value;
+  let speed = Math.pow(2,7-v);
+  let arrayBars = speed===1 ? 1000 : Math.round(1200/(speed+1))
 
+  this.resetArray(arrayBars)
+  this.setState({speed,arrayBars},()=>{
+    const arrBars = document.getElementsByClassName('array-bar');
+  for(let i=0;i<arrBars.length;i++){
+  arrBars[i].style.width = `${speed}px`
+  arrBars[i].style.margin = speed===1 ? 0 : "0 0.5px"
+  }
+})
+ 
 }
 
    
@@ -206,12 +238,12 @@ handleSlideChange=(e)=>{
         ))}
        </div>
         <div className="btn-container">
-         <button onClick={()=>this.resetArray()} className="btn">New Array</button>
+         <button onClick={()=>this.resetArray(this.state.arrayBars)} className="btn">New Array</button>
          <button onClick={()=>this.mergeSort()} className="btn">Merge Sort</button> 
          <button onClick={()=>this.bubbleSort()} className="btn">Bubble Sort</button>
          <button onClick={()=>this.insertionSort()} className="btn">Insertion Sort </button>
          <button onClick={()=>this.quickSort()} className="btn">Quick Sort </button>
-         <input type="range" min="1" max="100" step="1" value={this.state.speed} onChange={this.handleSlideChange} className="slider" id="myRange"></input>
+         <input type="range" min="0" max="7" step="1" value={7-Math.round(Math.log2(this.state.speed))} onChange={this.handleSlideChange} className="slider" id="myRange"></input>
         </div>
         </div>
     )
